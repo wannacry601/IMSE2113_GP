8aa2c774435992dd40cda5eb2e37edecd7cff6c8
wannacry601 <61106477+wannacry601@users.noreply.github.com>
2023-04-21 15:16:57 +0800

    Revert "Preliminary work complete"

--- AssetMgr/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='index'),
-     path('', include(routers.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AssetMgr',
-    'api',
-
 ]
 
 MIDDLEWARE = [
@@ -124,12 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
-
