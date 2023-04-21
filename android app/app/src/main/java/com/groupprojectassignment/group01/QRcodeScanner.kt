85e0f8533a2f13a0605b28bfc991ece2ef7f96f8
wannacry601 <sora@connect.hku.hk>
2023-04-21 15:21:05 +0800

    android app initialized

---
+++ android app/app/src/main/java/com/groupprojectassignment/group01/QRcodeScanner.kt
@@ -0,0 +1,12 @@
+package com.groupprojectassignment.group01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class QRcodeScanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode_scanner)
+    }
+}
---
+++ android app/app/src/main/java/com/groupprojectassignment/group01/databaselinker.kt
@@ -0,0 +1,11 @@
+package com.groupprojectassignment.group01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.database.sqlite.SQLiteOpenHelper
+
+class databaselinker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
