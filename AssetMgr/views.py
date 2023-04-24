028770b5d0d4350b8c6a609dff0ed1bfae165e67
HannahHK0622 <kanyiuyeung@gmail.com>
2023-04-24 12:49:41 +0800

    Serializer changes: Add login, logout, check_auth

--- AssetMgr/views.py
+++ AssetMgr/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from django.contrib.auth import login, logout
+from rest_framework import viewsets, permissions, views
+from rest_framework.response import Response
 from .serializers import *
 from . import models
 
@@ -24,3 +26,24 @@ class ProductsViewset(viewsets.ModelViewSet):
 class PelletViewset(viewsets.ModelViewSet):
     queryset = Pellet.objects.all()
     serializer_class = PelletSerializer
+
+class LoginViewset(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request, format=None):
+        serializer = LoginSerializer(data=self.request.data,
+            context={ 'request': self.request })
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response({'message': 'successfully logged in', 'user': user, 'auth': True}, status=202)
+
+class LogoutViewset(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Logged out', 'auth': False})
+
+class CheckAuth(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+    def get(self, request):
+        return Response({'is_authenticated': request.user.is_authenticated})
